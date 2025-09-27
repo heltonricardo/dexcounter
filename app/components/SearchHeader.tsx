c6a83ec3ac4206a330fef40b158a19ea18b5e141
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2025-09-27 14:39:26 -0300

    feat: add credits

--- app/components/SearchHeader.tsx
+++ app/components/SearchHeader.tsx
@@ -16,14 +16,40 @@ export default function SearchHeader({
 }: SearchHeaderProps) {
     return (
         <div className="text-center mb-8 px-2 sm:px-4">
-            <Link
-                href="/"
-                className="text-5xl sm:text-6xl md:text-8xl font-bold text-gray-800 mb-2"
-            >
-                DexCounter
-            </Link>
+            <h1>
+                <Link
+                    href="/"
+                    className="text-5xl sm:text-6xl md:text-8xl font-bold text-gray-800 mb-2"
+                >
+                    DexCounter
+                </Link>
+            </h1>
             <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6">
-                Explore Pokémon weaknesses, resistances, and stats
+                Explore Pokémon weaknesses, resistances, and stats •{" "}
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://helton.vercel.app"
+                    className={`
+                        inline-flex items-center
+                        hover:text-gray-900 transition-colors duration-200"`}
+                >
+                    By Helton
+                    <svg
+                        className="w-3 h-3 ml-1"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                    >
+                        <path
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d={`M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0
+                                0v6m0-6L10 14`}
+                        />
+                    </svg>
+                </a>
             </p>
 
             <div className="max-w-md sm:max-w-lg mx-auto relative">
